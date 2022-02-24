5c7cda8294f14ab31b2bfa0e0798436e22d3eba2
LastEternity <97479464+LastEternity@users.noreply.github.com>
2022-02-24 15:42:03 -0800

    Added snippets to test.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -23,6 +23,10 @@ public class MarkdownParseTest {
         fileMap.put("test-file2.md", new ArrayList<String>(Arrays.asList()));
         fileMap.put("test-file3.md", new ArrayList<String>(Arrays.asList()));
         fileMap.put("test-file4.md", new ArrayList<String>(Arrays.asList("url2")));
+        
+        fileMap.put("snippet3.md", new ArrayList<String>(Arrays.asList("https://ucsd-cse15l-w22.github.io/")));
+        fileMap.put("snippet2.md", new ArrayList<String>(Arrays.asList("a.com", "a.com(())", "example.com")));
+        fileMap.put("snippet1.md", new ArrayList<String>(Arrays.asList("`google.com", "google.com", "ucsd.edu")));
     }
 
     @Test
@@ -38,6 +42,21 @@ public class MarkdownParseTest {
     @Test
     public void testFile3() throws Exception {
         assertEquals(fileMap.get("test-file3.md"), MarkdownParse.getLinks(Files.readString(Path.of("test-file3.md"))));
+    }
+    
+    @Test
+    public void snippet1.md throws Exception {
+        assertEquals(fileMap.get("snippet1.md"), MarkdownParse.getLinks(Files.readString(Path.of("snippet1.md"))));
+    }
+    
+    @Test
+    public void snippet2.md throws Exception {
+        assertEquals(fileMap.get("snippet2.md"), MarkdownParse.getLinks(Files.readString(Path.of("snippet2.md"))));
+    }
+    
+    @Test
+    public void snippet3.md throws Exception {
+        assertEquals(fileMap.get("snippet3.md"), MarkdownParse.getLinks(Files.readString(Path.of("snippet3.md"))));
     }
 
     /*
